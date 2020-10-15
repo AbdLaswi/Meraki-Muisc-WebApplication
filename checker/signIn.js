a1fd14b73d6ba2d44779aae6a6c96e759ac2264b
Abdulrhman M las <abed,laswii@gmail.com>
2020-10-15 20:51:49 +0300

    First edition

---
+++ checker/signIn.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { users, roles } = require("../Users Data/userData.js");
+const login = async (user) => {
+    const savedUser = users.filter((usr) => usr.email === user.email);
+    if (savedUser.length) {
+      if (await bcrypt.compare(user.password, savedUser[0].password)) {
+        const permissions = roles.filter(
+          (role) => role.id === savedUser[0].role_id
+        );
+  
+        const payload = {
+          email: savedUser[0].email,
+          permissions: permissions[0].permissions,
+        };
+  
+        const options = {
+          expiresIn: process.env.TOKEN_EXPIRATION,
+        };
+  
+        return jwt.sign(payload, process.env.SECRET, options);
+      }
+    }
+    return "Invalid login";
+  };
+  
+  const getUsers = () => {
+    return users;
+  };
