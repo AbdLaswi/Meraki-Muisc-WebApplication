de666b237335c1d219d38153392be91df1702699
Abdulrhman M las <abed,laswii@gmail.com>
2020-10-16 12:55:13 +0300

    remove conosle.logs

--- Search/dataSearch.js
+++ Search/dataSearch.js
@@ -1,16 +1,18 @@
 const axios = require("axios");
-
-
-
+const Options = require("../DataBase/database");
 const search = (songName) => {
-    options.params.term = songName;
-    axios
-      .request(options)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-        throw error;
-      });
+  options.params.term = songName;
+  axios
+    .request(options)
+    .then((response) => {
+
+      return response.data;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+module.exports = {
+   search,
   };
+  
